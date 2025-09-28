1c9f07d301d6e1a6ace9ef7aa9900bf0891df764
Sharath Gururaj <e271828r@gmail.com>
2025-09-28 19:14:34 +0530

    foo

--- imports.jl
+++ imports.jl
@@ -3,4 +3,4 @@ import Pkg; Pkg.add("LibPQ"); Pkg.add("Tables"); Pkg.add("Plots"); Pkg.add("HTTP
 Pkg.add("Random"); Pkg.add("StatsBase"); Pkg.add("FreqTables"); Pkg.add("SQLite"); 
  Pkg.add("Dates"); Pkg.add("JuliaDB"); Pkg.add("StatsPlots"); Pkg.add("Statistics"); Pkg.add("StructArrays"); 
 Pkg.add("NPFinancial"); Pkg.add("Distributions"); Pkg.add("DataStructures");Pkg.add("PlotlyJS"); Pkg.add("HTTP");Pkg.add("ReusePatterns");Pkg.add("ActuaryUtilities");
-Pkg.add("TypeTree");Pkg.add("DataFrames")
+Pkg.add("TypeTree");Pkg.add("DataFrames");Pkg.add("JuliaDB")
